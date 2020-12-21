87b240cdd807d05719c24350ab9c2ec5e4f9005f
Jack Glenn <jglenn1038@gmail.com>
2020-12-21 12:53:02 -0600

    Comments added

--- BoardState.py
+++ BoardState.py
@@ -1,5 +1,9 @@
 class BoardState:
     def __init__(self):
+        #Saves the current board state
+        #"-" represents an empty square
+        #"w" represents a white disk
+        #"b" represents a black disk
         self._board = [
             ["-", "-", "-", "-", "-", "-", "-", "-"],
             ["-", "-", "-", "-", "-", "-", "-", "-"],
@@ -10,12 +14,13 @@ class BoardState:
             ["-", "-", "-", "-", "-", "-", "-", "-"],
             ["-", "-", "-", "-", "-", "-", "-", "-"]
             ]
-        self._possibleMoves = {
+        #List storing the index of all possible moves (not specific to a player)
+        self._possibleMoves = [
             (2, 2), (2, 3), (2, 4), (2, 5),
             (3, 2), (3, 5), 
             (4, 2), (4 ,5),
             (5, 2), (5, 3), (5, 4), (5, 5),
-        }
+        ]
         self._numberOfWhiteDisks = 2
 
     #Removes (y, x) from _possibleMoves and adds the applicable indices surrounding (y, x) to _possibleMoves
@@ -25,9 +30,10 @@ class BoardState:
                 curX = x + i
                 curY = y + j
                 if self._board[curY][curX] == '-':
-                    self._possibleMoves.add((curY, curX))
+                    self._possibleMoves.append((curY, curX))
         self._possibleMoves.remove((y, x))
 
+    #Checks if tiles can be swapped to color on the path from start to end, flips if possible and updates white disks.
     def positionChecker(self, start, end, color):
         y = start[0]
         x = start[1]
@@ -57,6 +63,7 @@ class BoardState:
                 return
             count += 1
             
+    #Defines the amount that a path should be indexed by (negative for left and up, positive for right and down)
     @staticmethod
     def __getChange(start, end):
         if (start == end):
@@ -65,7 +72,8 @@ class BoardState:
             return 1
         else:
             return -1
-        
+
+    #Makes all entries between start and end be the input color   
     def flipper(self, start, end, color):
         y = start[0]
         x = start[1]
